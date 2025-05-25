48a782b73354da19f676bf4e55bdcec3ddc09595
ipms_user_eng29 <IPMS_USR_ENG29@pln.co.id>
2025-05-25 09:26:53 +0700

    quick sort elixir (not optimized)

---
+++ quick_sort/quick_sort.exs
@@ -0,0 +1,69 @@
+defmodule QuickSort do
+  @doc """
+  Accept list of integer.
+  Choose the first member of the list as pivot.
+  Move lower number to the left and higher number to the right of pivot.
+  Do recursion on left and right sublist until their length is less than or equal.
+  Merge sub lists with each pivot and return {:ok, sorted_list}.
+  Return {:error, :not_list} if input is not a list.
+  Return {:error, :not_integer}
+  """
+
+  @spec sort(list(integer())) :: {:ok, list(integer())} | {:error, :not_list} | {:error, :not_integer}
+  def sort([]), do: {:ok, []}
+  def sort(arr) when not is_list(arr), do: {:error, :not_list}
+  def sort([head | tail]) when not is_number(head), do: {:error, :not_integer}
+
+  def sort(arr) do
+    {:ok, do_sort(arr)}
+  end
+
+  defp do_sort([]), do: []
+  defp do_sort([_] = arr), do: arr
+
+  defp do_sort([head | tail]) do
+    {left, right} = partition(head, tail, [], [])
+    do_sort(left) ++ [head | do_sort(right)]
+  end
+
+  defp partition(_, [], left_arr, right_arr) do
+    {Enum.reverse(left_arr), Enum.reverse(right_arr)}
+  end
+
+  defp partition(pivot, [head | tail], left_arr, right_arr) do
+    if head <= pivot do
+      partition(pivot, tail, [head | left_arr], right_arr)
+    else
+      partition(pivot, tail, left_arr, [head | right_arr])
+    end
+  end
+end
+
+ExUnit.start()
+
+defmodule TestQuickSort do
+  use ExUnit.Case, async: true
+
+  test "quick sort 1" do
+    assert QuickSort.sort([3, 7, 6, -10, 23, 15, 55, -13]) ==
+             {:ok, [-13, -10, 3, 6, 7, 15, 23, 55]}
+  end
+
+  test "quick sort 2" do
+    assert QuickSort.sort([7, 12, 9, 11, 3]) == {:ok, [3, 7, 9, 11, 12]}
+  end
+
+  test "quick sort 3" do
+    assert QuickSort.sort([10, 7, 8, 9, 1, 5]) == {:ok, [1, 5, 7, 8, 9, 10]}
+  end
+
+  test "quick sort 4" do
+    assert QuickSort.sort([5, 2, 9, 1, 5, 6]) == {:ok, [1, 2, 5, 5, 6, 9]}
+  end
+
+  test "quick sort 5" do
+    assert QuickSort.sort([]) == {:ok, []}
+    assert QuickSort.sort([1]) == {:ok, [1]}
+    assert QuickSort.sort(123) == {:error, :not_list}
+  end
+end
